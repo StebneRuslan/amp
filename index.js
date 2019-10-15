15a4b2074c98503907a9f8953e88756cf7e14d7e
Ruslan Bilmakovskiy <ruslan.bilmakovskyi@ekreative.com>
2019-10-15 13:48:39 +0300

    added analytics field

--- index.js
+++ index.js
@@ -78,8 +78,7 @@ let Amp = class Amp {
         let resultHtml = ''
         data.type = this.type
         let fullData = this.setConfigPath(data)
-        fullData.analytics = analytics
-        console.log(fullData.analytics)
+        fullData.analytics = 'werewrwerwerew'
         app.render(path.resolve(__dirname, 'views', 'ampViewer.ejs'), fullData, function(err, html) {
             if (err) {
                 console.log(err)
