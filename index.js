6523965fe4d161180a2ae591356e86b241e536d0
rbilmakovskyi <ruslan.bilmakovskyi@ekreative.com>
2020-01-27 12:22:43 +0200

    added code snippets

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-// const data = require('./src/data')
+const pick = require('lodash/pick')
 const express = require('express')
 const app = express()
 let Amp = class Amp {
@@ -9,13 +9,7 @@ let Amp = class Amp {
   }
 
   setConfigPath (data) {
-    data.type = this.type
-    data.v0Path = this.configPaths.v0Path
-    data.ampStoryPath = this.configPaths.ampStoryPath
-    data.ampVideoPath = this.configPaths.ampVideoPath
-    data.ampAnimationPath = this.configPaths.ampAnimationPath
-    data.fontPath = this.configPaths.fontPath
-    return data
+    return Object.assign(data, Object.assign({ type: this.type }, pick(this.configPaths, ['v0Path', 'ampStoryPath', 'ampVideoPath', 'ampAnimationPath', 'fontPath'])))
   }
 
   createSinglePage (data, calback) {
@@ -33,7 +27,7 @@ let Amp = class Amp {
 
   createStaticSinglePage (data, calback) {
     let resultHtml = ''
-    let fullData = this.setConfigPath(data)
+    let fullData = this.setConfigPath()
     app.render(path.resolve(__dirname, 'views', 'ampPupeteerSinglePage.ejs'), fullData, function (err, html) {
       if (err) {
         console.log(err)
@@ -46,7 +40,7 @@ let Amp = class Amp {
 
   createSinglePageWithAnimations (data, calback) {
     let resultHtml = ''
-    let fullData = this.setConfigPath(data)
+    let fullData = this.setConfigPath()
     app.render(path.resolve(__dirname, 'views', 'ampSinglePageWithAnimations.ejs'), fullData, function (err, html) {
       if (err) {
         console.log(err)
@@ -59,7 +53,7 @@ let Amp = class Amp {
 
   createSinglePageWithoutScripts (data, calback) {
     let resultHtml = ''
-    let fullData = this.setConfigPath(data)
+    let fullData = this.setConfigPath()
     app.render(path.resolve(__dirname, 'views', 'singlePageWithoutScripts.ejs'), fullData, function (err, html) {
       if (err) {
         console.log(err)
@@ -70,31 +64,48 @@ let Amp = class Amp {
     })
   }
 
-  createFullStory ({ config, userAnalytics = null, customerAnalyticsKey = null }, calback) {
+  createFullStory ({ config, userAnalytics = null, customerAnalyticsKey = null, snippets = [] }, callback) {
     let resultHtml = ''
     config.type = this.type
     let fullData = this.setConfigPath(config)
+    console.log('fullData', fullData)
     if (config.ampStory) {
       config.ampStory.googleAnalytics = userAnalytics
       config.ampStory.customerAnalyticsKey = customerAnalyticsKey
     }
+    config.snippets = snippets || []
     app.render(path.resolve(__dirname, 'views', 'ampViewer.ejs'), fullData, function (err, html) {
       if (err) {
         console.log(err)
       } else {
         resultHtml = html.replace(/\n/g, '')
       }
-      calback(resultHtml)
+      callback(resultHtml)
     })
   }
 }
 
 module.exports = Amp
+
+// const fs = require('fs')
+// const data = require('./src/data')
 // const amp = new Amp('cdn', {
-//     v0Path: 'https://cdn.ampproject.org/v0.js',
-//     ampStoryPath: 'https://cdn.ampproject.org/v0/amp-story-1.0.js',
-//     ampVideoPath: 'https://cdn.ampproject.org/v0/amp-video-0.1.js',
-//     ampAnimationPath: null,
-//     fontPath: '..'
+//   v0Path: 'https://cdn.ampproject.org/v0.js',
+//   ampStoryPath: 'https://cdn.ampproject.org/v0/amp-story-1.0.js',
+//   ampVideoPath: 'https://cdn.ampproject.org/v0/amp-video-0.1.js',
+//   ampAnimationPath: null,
+//   fontPath: '..'
 // })
-// let single = amp.createFullStory(data.data, null, (resultHtml) => {console.log('resultHtml')})
+//
+// const snippets = [{
+//   headCode: 'PHNjcmlwdCBhc3luYz0iIiBjdXN0b20tZWxlbWVudD0iYW1wLXZpZGVvIiBzcmM9Imh0dHBzOi8vY2RuLmFtcHByb2plY3Qub3JnL3YwL2FtcC12aWRlby0wLjEuanMxMTExMTExMTExMTExMTEiPjwvc2NyaXB0Pg==',
+//   bodyCode: ''
+// }, {
+//   headCode: 'PHNjcmlwdCBhc3luYz0iIiBjdXN0b20tZWxlbWVudD0iYW1wLXZpZGVvIiBzcmM9Imh0dHBzOi8vY2RuLmFtcHByb2plY3Qub3JnL3YwL2FtcC12aWRlby0wLjEuanMyMjIyMjIyMjIyIj48L3NjcmlwdD4=',
+//   bodyCode: ''
+// }]
+//
+// let single = amp.createFullStory({ config: data.data, userAnalytics: { trackingProductId: '', trackingAccountId: 'UA-97508922-1', email: '', ivwId: '', ivwComment: '', ivwCode: '' }, customerAnalyticsKey: 99999, snippets: snippets }, (resultHtml) => {
+//   // fs.writeFileSync('test.html', resultHtml)
+//   console.log('resultHtml', resultHtml)
+// })
